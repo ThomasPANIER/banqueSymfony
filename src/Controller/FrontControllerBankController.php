2caf45cf61ba0f923aae3289ccfdd7a2aa431c19
unknown <thomaspanier.pro@gmail.com>
2021-07-05 14:59:06 +0200

    add controller

---
+++ src/Controller/FrontControllerBankController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontControllerBankController extends AbstractController
+{
+    #[Route('/bank', name: 'bank')]
+    public function index(): Response
+    {
+        return $this->render('bank/index.html.twig', [
+            'controller_name' => 'FrontControllerBankController',
+        ]);
+    }
+}
