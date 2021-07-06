22387af05f9b74835310fd9b80c73deb89eba05a
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-07-06 14:56:06 +0200

    add register login and logout

--- src/Controller/FrontControllerBankController.php
+++ src/Controller/FrontControllerBankController.php
@@ -4,15 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Form\RegistrationFormType;
+
 
 class FrontControllerBankController extends AbstractController
 {
     #[Route('/bank', name: 'bank')]
+    #[Route('/', name: 'bank')]
     public function index(): Response
     {
-        return $this->render('front_controller_bank/index.html.twig', [
+        return $this->render('bank/index.html.twig', [
             'controller_name' => 'FrontControllerBankController',
         ]);
     }
+
+    
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,15 +18,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('address')
+            ->add('city')
+            ->add('postal')
+            ->add('tel')
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -42,6 +42,19 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('enregistrer', SubmitType::class, [
+                'attr' => ['class' => 'bg-dark text-white'],
+                'row_attr' => ['class' => 'text-center']
             ])
         ;
     }
