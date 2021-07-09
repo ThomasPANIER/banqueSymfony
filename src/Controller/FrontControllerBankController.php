725d88b00a25347e20fea688b1cc47a78311e3fe
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-07-09 14:44:15 +0200

    add lastOperation

--- src/Controller/FrontControllerBankController.php
+++ src/Controller/FrontControllerBankController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,10 +19,10 @@ use App\Repository\OperationRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
-    * Require ROLE_ADMIN for *every* controller method in this class.
-    *
-    * @IsGranted("IS_AUTHENTICATED_FULLY")
-    */
+ * Require ROLE_ADMIN for *every* controller method in this class.
+ *
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
 
 class FrontControllerBankController extends AbstractController
 {
@@ -31,32 +30,27 @@ class FrontControllerBankController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-
-        // $account = new Account();
-        // $id = $account->setUser($this->getUser());
         $accountRepository = $this->getDoctrine()->getRepository(Account::class);
         $accounts = $accountRepository->findby(
             ['user' => $this->getUser()],
         );
 
-        
         return $this->render('bank/index.html.twig', [
             'accounts' => $accounts,
         ]);
     }
 
     #[Route('/index/account/new', name: 'newAccount')]
-    public function newAccount(Request $request): Response 
+    public function newAccount(Request $request): Response
     {
         $account = new Account();
         $form = $this->createForm(AccountType::class, $account);
 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $account->setAccountNumber(mt_rand(100000, 200000));
             $account->setOpenDate(new \DateTime());
             $account->setUser($this->getUser());
-            //dump($account);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($account);
@@ -73,27 +67,45 @@ class FrontControllerBankController extends AbstractController
     #[Route('/index/account/{id}', name: 'singleAccount', requirements: ['id' => '\d+'])]
     public function singleAccount(int $id, OperationRepository $operationRepository, $operation = null): Response
     {
-
         $operationRepository = $this->getDoctrine()->getRepository(Operation::class);
         $operation = $operationRepository->find($id);
+        
         $accountRepository = $this->getDoctrine()->getRepository(Account::class);
         $account = $accountRepository->find($id);
 
-        
         return $this->render('bank/singleAccount.html.twig', [
             'account' => $account,
             'operation' => $operation,
         ]);
     }
 
+    #[Route('/index/account/{accountId}/lastOperation', name: 'lastOperation', requirements: ['accountId' => '\d+'])]
+    public function lastOperation(OperationRepository $operationRepository, AccountRepository $accountRepository, $operations = null, int $accountId): Response
+    {
+        $accountRepository = $this->getDoctrine()->getRepository(Account::class);
+        $account = $accountRepository->find($accountId);
+
+        $operationRepository = $this->getDoctrine()->getRepository(Operation::class, $operations);
+        $operations = $operationRepository->findBy(
+            ['account' =>  $account],
+        );
+
+        return $this->render('bank/lastOperation.html.twig', [
+            'account' => $account,
+            'operations' => $operations,
+        ]);
+    }
+
     #[Route('/index/account/{accountId}/credit', name: 'creditOperation', requirements: ['accountId' => '\d+'])]
-    public function creditOperation(Request $request, AccountRepository $accountRepository, int $accountId): Response 
+    public function creditOperation(Request $request, AccountRepository $accountRepository,  OperationRepository $operationRepository, int $accountId): Response
     {
         $credit = new Operation();
         $form = $this->createForm(CreditType::class, $credit);
 
+        $account = new Account();
+
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $credit->setOperationType("Crédit");
             $credit->setOperationDate(new \DateTime());
             $account = $accountRepository->find($accountId);
@@ -103,6 +115,12 @@ class FrontControllerBankController extends AbstractController
             $entityManager->persist($credit);
             $entityManager->flush();
 
+            $amountOpe = $credit->getOperationAmount($accountId);
+            $amountAccount = $account->getAccountAmount($accountId);
+            $newAmount = $account->setAccountAmount($amountOpe + $amountAccount);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush($newAmount);
+
             return $this->redirectToRoute('index');
         }
 
@@ -110,6 +128,4 @@ class FrontControllerBankController extends AbstractController
             'form' => $form->createView()
         ]);
     }
-    
 }
-
